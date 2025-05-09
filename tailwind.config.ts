367012a29d2ea8025513343621d34ccf122342d4
7jep7 <je77petersen@gmail.com>
2025-05-09 23:07:47 +0100

    added mobile version of the navbar with hamburger and x icons (transition animation can still be improved, esp with line thickness variance)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,15 @@ export default {
           "Noto Color Emoji",
         ],
       },
+      animation: {
+        'fade-slide': 'fadeSlide 0.3s ease-out forwards',
+      },
+      keyframes: {
+        fadeSlide: {
+          '0%': { opacity: '0', transform: 'translateY(-10px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
     },
   },
   plugins: [],
